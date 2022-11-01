8453838dbc2a622deca21e6f15e074f33bc7d4cf
howard <howard@richwhole.net>
2022-11-01 15:23:30 +0800

    tt

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const _ = require("lodash");
 
-const octokit = github.getOctokit();
+const token = core.getInput('token');
+const octokit = github.getOctokit(token);
 octokit.rest.actions.listArtifactsForRepo().then((data) => {
   _.each(data.artifacts, (i) => {
     core.info(`${i.name}-${i.expires_at}`);
