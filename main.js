ac31d1edfe0eb4cbcd7f242faf268a5f3d5be548
howard <howard@richwhole.net>
2022-11-01 15:29:35 +0800

    fix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const github = require("@actions/github");
 const _ = require("lodash");
 
 const token = core.getInput('token');
-const octokit = github.getOctokit();
+const octokit = github.getOctokit(token);
 octokit.rest.actions.listArtifactsForRepo().then((data) => {
   _.each(data.artifacts, (i) => {
     core.info(`${i.name}-${i.expires_at}`);
